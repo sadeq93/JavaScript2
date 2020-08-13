6a323cccecb3e3b9b54dfd7e99c2f59cad0099c9
Sadeq Al-Dallal <66870615+sadeq93@users.noreply.github.com>
2020-08-13 23:03:07 +0200

    added the project (pre final)

--- Week2/project/index.js
+++ Week2/project/index.js
@@ -1,10 +1,105 @@
-/**
-  In this week 's project you'll be making a Pomodoro Clock!
-  A user can specify how many minutes the timer should be set, and with a click on the play button it starts counting down!If the user wants to pause the timer, they can do so by clicking the pause button.
+const l = console.log;
 
-  If the timer is running, the user can 't change the session length anymore
-  Use at least 3 functions
-  Display minutes and seconds
-  If the timer finishes the timer should be replaced by the message: Time 's up!
- * 
- */
+
+const decreaseButton = document.getElementById("Change-Session-Length").firstElementChild;
+let sessionLength = decreaseButton.nextElementSibling;
+const increaseButton = sessionLength.nextElementSibling;
+// -------
+let session = document.getElementById("session").lastElementChild;
+session.textContent = sessionLength.textContent;
+
+
+// -------
+
+let playAndResetButton = document.getElementById("session-control").firstElementChild;
+let pauseButton = playAndResetButton.nextElementSibling;
+
+// Increase session length function.
+const increaseSession = () => {
+  sessionLength.textContent = parseFloat(sessionLength.textContent) + 1;
+  session.textContent = sessionLength.textContent;
+  timer = session.textContent * 60;
+}
+// decrease session length function.
+const decreaseSession = () => {
+  sessionLength.textContent = parseFloat(sessionLength.textContent) - 1;
+  session.textContent = sessionLength.textContent;
+  timer = session.textContent * 60;
+  if (sessionLength.textContent == 1) {
+    decreaseButton.disabled = true;
+
+  }else if (sessionLength.textContent == 2 )   {
+    decreaseButton.disabled = false;
+    }
+  
+  
+  
+
+   
+
+}
+// decrease the session (timer)second every second.
+let timer = session.textContent * 60;
+const countItDown = () => {
+  
+  minute = Math.floor(timer / 60);
+  seconds = timer % 60;
+  seconds = seconds < 10 ? `0${seconds}`:seconds;
+  session.textContent = `${minute}:${seconds}`;
+  timer--
+  
+  if (session.textContent === "0:00") {
+    decreaseButton.disabled = false;
+    increaseButton.disabled = false;
+    session.textContent = "Time's up!";
+    window.clearInterval(countItDownId);
+  }
+}
+
+
+let countItDownId;
+let flag;
+const playAndReset = () => {
+  flag = !flag;
+  if (!flag) {
+    window.clearInterval(countItDownId);
+    playAndResetButton.classList.add("play");
+    playAndResetButton.classList.remove("stop");
+    decreaseButton.disabled = false;
+    increaseButton.disabled = false;
+    session.textContent = sessionLength.textContent;
+    timer = session.textContent * 60;
+  }else if (session.textContent !== "Time's up!") {
+    countItDownId = window.setInterval(countItDown, 60);
+    playAndResetButton.classList.add("stop");
+    playAndResetButton.classList.remove("play");
+    decreaseButton.disabled = true;
+    increaseButton.disabled = true;
+
+  }
+}
+
+const pauseSession = () => {
+  flag = !flag
+  if (!flag) {
+    
+    window.clearInterval(countItDownId);
+    pauseButton.classList.add("pause");
+    pauseButton.classList.remove("play");
+  } else if (session.textContent !== "Time's up!"){
+   
+    countItDownId = window.setInterval(countItDown, 60);
+    pauseButton.classList.add("play");
+    pauseButton.classList.remove("pause");
+    decreaseButton.disabled = true;
+    increaseButton.disabled = true;
+  }
+}
+
+// Events
+let decrease = decreaseButton.addEventListener("click", decreaseSession);
+ let increase = increaseButton.addEventListener("click", increaseSession);
+playAndResetButton.addEventListener("click", playAndReset);
+pauseButton.addEventListener("click", pauseSession);
+
+
