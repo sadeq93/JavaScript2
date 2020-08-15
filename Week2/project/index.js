7037c59fa8cffdd6744c09175a082658fb65f4d2
Sadeq Al-Dallal <66870615+sadeq93@users.noreply.github.com>
2020-08-15 20:26:56 +0200

    finish JS2-week2 Homework

--- Week2/project/index.js
+++ Week2/project/index.js
@@ -1,24 +1,21 @@
-const l = console.log;
 
-
+// storing as variables
 const decreaseButton = document.getElementById("Change-Session-Length").firstElementChild;
-let sessionLength = decreaseButton.nextElementSibling;
+const sessionLength = decreaseButton.nextElementSibling;
 const increaseButton = sessionLength.nextElementSibling;
-// -------
-let session = document.getElementById("session").lastElementChild;
-session.textContent = sessionLength.textContent;
-
-
-// -------
-
-let playAndResetButton = document.getElementById("session-control").firstElementChild;
-let pauseButton = playAndResetButton.nextElementSibling;
+const session = document.getElementById("session").lastElementChild;
+// session.textContent = sessionLength.textContent;
+const playAndResetButton = document.getElementById("session-control").firstElementChild;
+const pauseButton = playAndResetButton.nextElementSibling;
 
 // Increase session length function.
 const increaseSession = () => {
   sessionLength.textContent = parseFloat(sessionLength.textContent) + 1;
   session.textContent = sessionLength.textContent;
   timer = session.textContent * 60;
+ if (sessionLength.textContent == 2) {
+  decreaseButton.disabled = false;
+} 
 }
 // decrease session length function.
 const decreaseSession = () => {
@@ -27,27 +24,20 @@ const decreaseSession = () => {
   timer = session.textContent * 60;
   if (sessionLength.textContent == 1) {
     decreaseButton.disabled = true;
-
-  }else if (sessionLength.textContent == 2 )   {
-    decreaseButton.disabled = false;
-    }
-  
-  
-  
-
-   
-
+    
+  }
 }
+
 // decrease the session (timer)second every second.
 let timer = session.textContent * 60;
 const countItDown = () => {
-  
+
   minute = Math.floor(timer / 60);
   seconds = timer % 60;
-  seconds = seconds < 10 ? `0${seconds}`:seconds;
+  seconds = seconds < 10 ? `0${seconds}` : seconds;
   session.textContent = `${minute}:${seconds}`;
   timer--
-  
+
   if (session.textContent === "0:00") {
     decreaseButton.disabled = false;
     increaseButton.disabled = false;
@@ -58,10 +48,10 @@ const countItDown = () => {
 
 
 let countItDownId;
-let flag;
+let condition;
 const playAndReset = () => {
-  flag = !flag;
-  if (!flag) {
+  condition = !condition;
+  if (!condition) {
     window.clearInterval(countItDownId);
     playAndResetButton.classList.add("play");
     playAndResetButton.classList.remove("stop");
@@ -69,8 +59,10 @@ const playAndReset = () => {
     increaseButton.disabled = false;
     session.textContent = sessionLength.textContent;
     timer = session.textContent * 60;
-  }else if (session.textContent !== "Time's up!") {
-    countItDownId = window.setInterval(countItDown, 60);
+  } else if (session.textContent !== "Time's up!") {
+    session.textContent = sessionLength.textContent;
+    timer = session.textContent * 60;
+    countItDownId = window.setInterval(countItDown, 1000);
     playAndResetButton.classList.add("stop");
     playAndResetButton.classList.remove("play");
     decreaseButton.disabled = true;
@@ -80,15 +72,15 @@ const playAndReset = () => {
 }
 
 const pauseSession = () => {
-  flag = !flag
-  if (!flag) {
-    
+  condition = !condition
+  if (!condition) {
+
     window.clearInterval(countItDownId);
     pauseButton.classList.add("pause");
     pauseButton.classList.remove("play");
-  } else if (session.textContent !== "Time's up!"){
-   
-    countItDownId = window.setInterval(countItDown, 60);
+  } else if (session.textContent !== "Time's up!") {
+
+    countItDownId = window.setInterval(countItDown, 1000);
     pauseButton.classList.add("play");
     pauseButton.classList.remove("pause");
     decreaseButton.disabled = true;
@@ -98,7 +90,7 @@ const pauseSession = () => {
 
 // Events
 let decrease = decreaseButton.addEventListener("click", decreaseSession);
- let increase = increaseButton.addEventListener("click", increaseSession);
+let increase = increaseButton.addEventListener("click", increaseSession);
 playAndResetButton.addEventListener("click", playAndReset);
 pauseButton.addEventListener("click", pauseSession);
 
