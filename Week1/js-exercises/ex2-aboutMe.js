c97b7958e880f1dd564ef5fda44d347e2136ab00
Sadeq Al-Dallal <66870615+sadeq93@users.noreply.github.com>
2020-08-19 19:10:29 +0200

    Update ex2-aboutMe.js

--- Week1/js-exercises/ex2-aboutMe.js
+++ Week1/js-exercises/ex2-aboutMe.js
@@ -1,4 +1,4 @@
-const body = document.querySelector('body');
+const body = document.body;
 body.style.fontFamily = "Arial, sans-serif";
 const ul = body.children[1].children;
 let myInfo = ['Sadeq', 'Pizza','Nieuwkoop']
