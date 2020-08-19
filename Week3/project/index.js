45bf8118169371c25a9fc0910b5f396e21ad197a
Sadeq Al-Dallal <66870615+sadeq93@users.noreply.github.com>
2020-08-19 20:16:15 +0200

    pre final (tip calculator project)

--- Week3/project/index.js
+++ Week3/project/index.js
@@ -1,3 +1,41 @@
-// Your code goes in here
+// document.querySelector("#app").innerText = "Tip Calculator";
+const calculateButton = document.getElementById('calculate-button');
+const CalculateTip = () => {
+  const amount = document.getElementById('amount');
+  const service = document.getElementById('service');
+  const sharingPeople = document.getElementById('sharingPeople');
+  const result = document.getElementById('result');
 
-document.querySelector("#app").innerText = "Tip Calculator";
+  let tipAmount = (parseFloat(amount.value) + (parseFloat(amount.value) / 100) * parseFloat(service.value)) / parseFloat(sharingPeople.value);
+  let amountFlag = false;
+  let sharingPeopleFlag = false;
+  if (amount.value === '' || service.value === '' || sharingPeople.value === '') {
+    alert('Fill in all the fields!')
+  } else {
+    if (!isNaN(amount.value)) {
+      amountFlag = true
+    } else {
+      amount.value = `invalid Number`
+      amount.style.color = 'red';
+    }
+
+    if (!isNaN(sharingPeople.value)) {
+      sharingPeopleFlag = true
+    } else {
+      sharingPeople.value = `invalid Number`
+      sharingPeople.style.color = 'red';
+    }
+    if (amountFlag === true && sharingPeopleFlag === true) {
+      if (parseFloat(sharingPeople.value) !== 1) {
+        result.textContent = `TIP AMOUNT $${tipAmount} each`
+      } else {
+        result.textContent = `TIP AMOUNT $${tipAmount}`
+      }
+    
+    }
+  }
+  console.log(amount.value);
+}
+
+calculateButton.addEventListener("click", CalculateTip);
+
